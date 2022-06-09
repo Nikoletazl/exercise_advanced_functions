6b1025d137727e83ef5879205e921cabd626951c
Nikoleta Zlateva <87802876+Nikoletazl@users.noreply.github.com>
2022-06-09 21:02:29 +0300

    first commit

---
+++ arguments.js
@@ -0,0 +1,21 @@
+function solve(){
+    const params = Array.from(arguments);
+
+    const types = {}
+
+    for (let arg of params) {
+        const type = typeof arg;
+        console.log(`${typeof arg}: ${arg}`)
+        if (types[type] == undefined) {
+            types[type] = 0;
+        }
+        types[type]++;
+    }
+    const result = Object.entries(types).sort((a, b) => b[1] - a[1])
+
+    for (let [type, count] of result) {
+        console.log(`${type} = ${count}`)
+    }
+}
+
+solve('cat', 42, function () { console.log('Hello world!'); })
